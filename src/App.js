1f4c41ea60c43307eee59eeaa168a77468eb21d2
Bjim <imbjim1@gmail.com>
2017-06-08 23:06:53 +0200

    Use map to Create React Components from Arrays of Data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 /*jshint esversion: 6 */
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
-  super();
-  this.state = {increasing: false}
-}
-  update() {
-    ReactDOM.render(
-      <App val={this.props.val+1} />,
-      document.getElementById('root'))
-  }
-  componentWillReceiveProps(nextProps){
-  this.setState({increasing: nextProps.val > this.props.val})
-}
-  shouldComponentUpdate(nextProps, nextState) {
-  return nextProps.val % 5 === 0;//update if val is a multiple of 5
-}
-  render(){
-    console.log(this.state.increasing)
-  return (
-    <button onClick={this.update.bind(this)}>
-      {this.props.val}
-    </button>
-  )
+      super();
+      this.state = {items: []}
+    }
+    componentWillMount(){//AJAX call to the Star Wars API:
+      fetch('http://swapi.co/api/people/?format=json')
+        .then( response => response.json() ) //we get the data back
+        .then( ({results: items}) => this.setState({items})) // we call the data items and set our state of items to that value.
+    }
+    filter(e){
+      this.setState({filter: e.target.value})
+    }
+    render(){//the item.name key here is to get rid of an error message about the necessity of having a key for each child
+      let items = this.state.items; //setting the data up
+      if(this.state.filter){
+        items = items.filter( item =>
+        item.name.toLowerCase()
+          .includes(this.state.filter.toLowerCase()))
+      }
+      return (
+        <div>
+          <input type="text"
+          onChange={this.filter.bind(this)}/>
+          {items.map(item =>
+            <Person key={item.name} person={item} />)}
+        </div>
+      )
  }
- componentDidUpdate(prevProps, prevState) {
-  console.log(`prevProps: ${prevProps.val}`)
- }
+
 }
 
-App.defaultProps = {val: 0}
+const Person = (props) => <h4>{props.person.name}</h4>
 
 export default App
