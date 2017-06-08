ee2c2e796bb63754adb753e7c2744eb8102c5bab
Bjim <imbjim1@gmail.com>
2017-06-08 14:28:45 +0200

    Manage React Component State with setState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 /*jshint esversion: 6 */
 import React from 'react';
 class App extends React.Component {
+  constructor(){
+    //unlike props, state is for values meant to be managed & updated by our components
+    super(); //super gives 'this' the context within our component
+    this.state = {
+      txt:'this is the state txt',
+      cat: 0
+    }
+  }
+
+  update ( e ){
+    this.setState({txt: e.target.value});
+    //if we want to update cat as well, we need this:
+    //this.setState({cat: e.target.value});
+  }
+
+
   render(){
-    let txt = this.props.txt;
-    return (
+
+    return (//two-way binding:
       <div>
-        <h1 className="murde">{txt}</h1>
+        <h1 className="murde">{this.state.txt} - {this.state.cat}</h1>
+        <input type="text" onChange={this.update.bind(this)}/>
       </div>
     )
   }
 }
 
-App.PropTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default" //this will show up only if App doesn't have a txt set in index.js
-}
 
 
 
