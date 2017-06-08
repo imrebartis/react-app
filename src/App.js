0e3931c4af59672f317df85d7a0d13510d6170c1
Bjim <imbjim1@gmail.com>
2017-06-08 23:35:30 +0200

    Compose React Component Behavior with Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 /*jshint esversion: 6 */
 import React from 'react';
 
-class App extends React.Component {
-  constructor() {
-      super();
-      this.state = {items: []}
-    }
-    componentWillMount(){//AJAX call to the Star Wars API:
-      fetch('http://swapi.co/api/people/?format=json')
-        .then( response => response.json() ) //we get the data back
-        .then( ({results: items}) => this.setState({items})) // we call the data items and set our state of items to that value.
-    }
-    filter(e){
-      this.setState({filter: e.target.value})
-    }
-    render(){//the item.name key here is to get rid of an error message about the necessity of having a key for each child
-      let items = this.state.items; //setting the data up
-      if(this.state.filter){
-        items = items.filter( item =>
-        item.name.toLowerCase()
-          .includes(this.state.filter.toLowerCase()))
-      }
-      return (
-        <div>
-          <input type="text"
-          onChange={this.filter.bind(this)}/>
-          {items.map(item =>
-            <Person key={item.name} person={item} />)}
-        </div>
-      )
- }
+//The function of a higher order component (HOC) is to take in a component and return a new component:
 
+const HOC = (InnerComponent) => class extends React.Component {
+  constructor(){
+    super();
+    this.state = {count: 0}
+  }
+  update(){
+    this.setState({count: this.state.count + 1})
+  }
+  componentWillMount(){
+   console.log('will mount')
+  }
+  render(){
+    return (
+      <InnerComponent
+      {...this.props}
+      {...this.state}
+      update={this.update.bind(this)}
+      />
+    )
+  }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+Class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Button>button</Button>
+        <hr/>
+        <LabelHOC>label</LabelHOC>
+      </div>
+    )
+  }
+}
+
+const Button = HOC((props)) =>
+ <button onClick={props.update}>{props.children} - {props.count}</button>
+class Label extends React.Component {
+  componentWillMount() {
+    console.log('label will mount')
+  }
+  render(){
+    return (
+      <label onMouseMove={this.props.update}>
+      {this.props.children} - {this.props.count>
+      </label>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label)
 
 export default App
