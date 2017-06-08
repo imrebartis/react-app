86088433f4397ae0723ffc204f9eba274f12e3a2
Bjim <imbjim1@gmail.com>
2017-06-08 15:43:20 +0200

    Use React ref to Get a Reference to Specific Components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 /*jshint esversion: 6 */
 import React from 'react';
+import ReactDOM from 'react-dom'; //we need this import in order to make the Input component work with ref a
 class App extends React.Component {
+
   constructor(){
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
-  update(e){
-    this.setState({currentEvent: e.type})
+  update(){
+    this.setState({//thanks to this the two inputs and their states can be modified in the browser separately
+      //we use two diff ways to get a reference to a component, see a and b
+      a: this.a.refs.input.value, //the input here points to the ref='input' of the Input component
+      b: this.refs.b.value
+    })
   }
   render(){
     return ( //the h1 will print out the name of the event, e.g. paste
       <div>
-        <textarea
-           onKeyPress={this.update}
-           onCopy={this.update}
-           onCut={this.update}
-           onPaste={this.update}
-           onFocus={this.update}
-           onBlur={this.update}
-           onMouseOver={this.update}
-           onDoubleClick={this.update}
-           onTouchStart={this.update}
-           onTouchMove={this.update}
-           onTouchEnd={this.update}
-           cols="30"
-           rows="10"/>
-        <h1>{this.state.currentEvent}</h1>
+        <Input
+          ref={ component => this.a = component}
+          update={this.update.bind(this)}
+        //ref={node => this.a = node} //this was used before creating the Input component
+          //type="text"
+          //onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+        ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
       </div>
     )
   }
 }
 
+class Input extends React.Component {
+  render(){
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
+}
 
 
 export default App
