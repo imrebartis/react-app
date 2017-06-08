da3e12593d7d8605394558af749638c80f0d6adc
Bjim <imbjim1@gmail.com>
2017-06-08 14:36:11 +0200

    Use React Components as Children for Other Components

--- src/App.js
+++ src/App.js
@@ -2,33 +2,32 @@
 import React from 'react';
 class App extends React.Component {
   constructor(){
-    //unlike props, state is for values meant to be managed & updated by our components
-    super(); //super gives 'this' the context within our component
+    super();
     this.state = {
       txt:'this is the state txt',
-      cat: 0
     }
   }
 
   update ( e ){
     this.setState({txt: e.target.value});
-    //if we want to update cat as well, we need this:
-    //this.setState({cat: e.target.value});
   }
 
 
   render(){
-
-    return (//two-way binding:
+    return (
       <div>
-        <h1 className="murde">{this.state.txt} - {this.state.cat}</h1>
-        <input type="text" onChange={this.update.bind(this)}/>
+        <h1 className="murde">{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
+        <Widget update={this.update.bind(this)} />
       </div>
     )
   }
 }
 
-
+//The stateless function component Widget is a child component of the React component, it updates the state of its parent:
+const Widget = (props) =>
+  <input type="text" onChange={props.update}/>
 
 
 export default App
