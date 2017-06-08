32cb86a83ce9e4a9c070b8196cac50ce96881c63
Bjim <imbjim1@gmail.com>
2017-06-08 14:01:25 +0200

    Display Output in React with a Component's render Method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 /*jshint esversion: 6 */
 import React from 'react';
-// class App extends React.Component {//the comments below are diff ways of rendering the h1
-//   render(){
-//     return React.createElement('h1', null,'hurdel gurdel' )
-//     //unlike in html, class is here className
-//     //return <h1 className="murde">Hurde Gurde</h1>
-//   }
-// }
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1 className="murde">Hurde Gurde</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
-//this is a stateless function component, not as useful as the class App solution:
-const App = () => <h1>Hurde gurde</h1>
+
 export default App
